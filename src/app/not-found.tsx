d1c27e5d2c998a50f54159f0d9d4fe982c6021ad
gleidson fagno <gleidsonfagner838@gmail.com>
2025-03-04 19:42:46 -0300

    feat: Mudano projeto par next

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+
+
+export default function NotFound() {
+  return (
+      <div>
+      <h1>Desculpe, não encontramos o que  você ta pesquisado</h1>
+      </div>
+  )
+}
---
+++ src/components/layouts/navbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="flex flex-col items-center justify-center gap-8 mb-20">
+      <Image
+        src="/Logo-Carnaval-Horizontal.png"
+        alt="logo do carnaval "
+        width={319}
+        height={54}
+      />
+
+      <nav
+        className="flex gap-8 justify-center bg-[#FFFF00] w-full"
+        aria-label="Paginação  principal"
+      >
+        <Link
+          href="#"
+          className="px-8 py-2 border-l border-r border-black text-orange-600 text-lg font-bold font-poppins"
+        >
+          HOME
+        </Link>
+          <Link
+            href="#"
+            className="px-8 py-2 border-l border-r border-black text-orange-600 text-lg font-bold font-poppins"
+          >BLOCOS</Link>
+      </nav>
+    </header>
+  );
+}
