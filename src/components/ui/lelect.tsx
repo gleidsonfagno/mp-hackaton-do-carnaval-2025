f025923eeb0d76a03f8c10ac2c3e1815094e924e
gleidson fagno <gleidsonfagner838@gmail.com>
2025-03-07 11:00:01 -0300

    feat: filtro por Cidades

--- src/components/ui/lelect.tsx
+++ src/components/ui/lelect.tsx
@@ -1,24 +1,51 @@
 "use client"
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+ 
+
 export function City() {
+
+  
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const {replace} = useRouter()
+
+  // Pegando a cidade da URL para deixar  o  valor lecionado nao mudar mesmo com reload
+  const selectedCity = searchParams.get("city") || "";
+
+  function handleChangeFillter(event: React.ChangeEvent<HTMLSelectElement>)  {
+    const params = new URLSearchParams(searchParams)
+
+    const selectCity = event.currentTarget.value
+
+    if(selectCity){
+      params.set("city", selectCity)
+    }else {
+      params.delete("search")
+    }
+
+    replace(`${pathname}?${params.toString()}`, {scroll: false})
+  }
+
   return (
     <select
-      name="city"
-      id="city"
-      
       className="p-2.5  border border-[#666666] text-[#666666] w-fit"
+      onChange={handleChangeFillter}
+      value={selectedCity}
     >
       <option value="" disabled>
         Selecione uma cidade
       </option>
-      <option value="sao-paulo">São Paulo</option>
-      <option value="rio-de-janeiro">Rio de Janeiro</option>
-      <option value="belo-horizonte">Belo Horizonte</option>
-      <option value="salvador">Salvador</option>
-      <option value="recife-olinda">Recife e Olinda</option>
-      <option value="brasilia">Brasília</option>
-      <option value="fortaleza">Fortaleza</option>
-      <option value="porto-alegre">Porto Alegre</option>
+      <option value="São Paulo">São Paulo</option>
+      <option value="Rio de Janeiro">Rio de Janeiro</option>
+
+      <option value="Belo Horizonte">Belo Horizonte</option>
+      <option value="Salvador">Salvador</option>
+      <option value="Recife e Olinda">Recife e Olinda</option>
+      <option value="Brasilia">Brasília</option>
+      <option value="Fortaleza
+">Fortaleza</option>
+      <option value="Porto Alegre">Porto Alegre</option>
     </select>
   );
 }
