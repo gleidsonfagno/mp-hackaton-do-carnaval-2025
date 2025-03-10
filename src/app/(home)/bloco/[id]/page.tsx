413e40f4515161dc4b6b204c732e20de31c07cd2
gleidson fagno <gleidsonfagner838@gmail.com>
2025-03-10 12:12:20 -0300

    feat: adiciona página de detalhes para os blocos

--- src/app/(home)/bloco/[id]/page.tsx
+++ src/app/(home)/bloco/[id]/page.tsx
@@ -1,42 +1,70 @@
 // import { Detail } from "@/components/layouts/detail";
-// "user client"
+"user client";
+// import axios from "axios";
 import Link from "next/link";
 
-export default async function Details() {
+export default async function Details({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const bloco_id = (await params).id;
 
+  // Fazendo a requisição para obter os blocos da API
+  const response = await fetch(
+    `https://apis.codante.io/api/bloquinhos2025/agenda`
+  );
 
+  if (!response.ok) {
+    throw new Error("Erro ao buscar dados da API");
+  }
 
+  const data = await response.json();
+
+  // Filtrando o bloco pelo ID correto
+  const bloco = data.data.find(
+    (bloco: { id: number }) => bloco.id === Number(bloco_id)
+  );
+
+  // Se não encontrou o bloco, retorna erro
+  if (!bloco) {
+    return <p>Bloco não encontrado.</p>;
+  }
+
+  console.log(bloco)
+
+  // console.log((await params).id)
   return (
     <div>
-        <p>paramentros  </p>
+      <p>{bloco.id} </p>
       <Link rel="stylesheet" href="/">
         voltar
       </Link>
 
       <article className="flex flex-col gap-8 mb-">
         <h2 className="text-4xl text-orange-600 font-semibold">
-          9º Ensaio da Bateria Emilias e Viscondes
+          {bloco?.title}
         </h2>
         <p className="text-[#616161] text-[16px] font-medium">
-          Juntos, vamos fazer do Carnaval 2025 uma festa inesquecível para a
-          criançada! Com muita música, dança, fantasia e alegria, vamos celebrar
-          a magia do Carnaval e criar memórias que vão durar para sempre!
+          {bloco?.description}
         </p>
 
-        <div className="description-addres">
+        <div className="description-addres flex flex-row gap-5">
           <span className="text-[#616161] text-[16px] font-medium">
-            Rua General Jardim, 485, Vila Buarque - São Paulo - SP
+            {bloco?.complete_address}
           </span>
 
           <span className="text-[#616161] text-[16px] font-medium">
-            2025-02-28T18
+            {bloco?.date_time}
           </span>
 
-          <span className="text-[#616161] text-[16px] font-medium">Grátis</span>
+          <span className="text-[#616161] text-[16px] font-medium">
+            {bloco?.price}
+          </span>
         </div>
 
         <a href="" className="text-[#2564CF] text-[16px] font-medium">
-          https://www.instagram.com/p/DFGibHlxQVr/?igsh=dzlpdHNvM29uNzNi
+          {bloco?.event_url}
         </a>
       </article>
       {/* <Detail /> */}
